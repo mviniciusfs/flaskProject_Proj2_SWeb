cdb017351c10a48e52eeb6d205118268227fd527
marcos.fs <viniciusfigueiredo@ucl.br>
2021-10-16 13:56:41 -0300

    Att

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def bible():  # put application's code here
     pesquisa = 'João+3:16?translation=almeida'
-    resposta = requests.get('https://bible-api.com/' + pesquisa).json()['reference'],['text']
+    resposta = requests.get('https://bible-api.com/' + pesquisa)
+    livro = resposta.json()['reference']
+    verso = resposta.json()['text']
+    livro_vers = livro+' - '+verso
 
-    return render_template('search.html', resposta=resposta)
+    return render_template('search.html', livro_vers=livro_vers)
 
 
 if __name__ == '__main__':
